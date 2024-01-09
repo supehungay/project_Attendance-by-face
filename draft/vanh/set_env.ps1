87e940b4698fad80a3a0ae2450465a69b38c7cb6
VanhhNe <vietanhnd.2002@gmail.com>
2024-01-09 22:26:45 +0700

    add env name

---
+++ draft/vanh/set_env.ps1
@@ -0,0 +1,4 @@
+$Env:DB_URL = "https://attendance-by-face-7de0b-default-rtdb.firebaseio.com/"
+$Env:STR_URL = "attendance-by-face-7de0b.appspot.com"
+$Env:TEMPLATE = "D:\\dulieuD\\Program Language\\Computer_Vision\\FinalExam\\project_Attendance-by-face\\image\\input\\template2.png"
+$Env:CERD = "D:\\dulieuD\\Program Language\\Computer_Vision\\FinalExam\\project_Attendance-by-face\\serviceAccountKey.json"
